544f1944c8626855bcd943a143917725fbd4a3e2
ahmedreda <ahmedreda7296@gmail.com>
2021-03-11 04:55:26 +0200

    prepare for deplyment

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import movies from "./routes/movies.js";
 import users from "./routes/users.js";
 import cors from "cors";
+import path from "path";
 const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
@@ -14,14 +15,18 @@ const CONNECTIONURL =
 
 app.use("/api/movies", movies);
 app.use("/api/users", users);
-// app.use((err, req, res, next) => {
-//   //  res.send(err.returnstatus);
+const __dirname = path.resolve();
 
-//   // Sends response
-//   return res.json({ message: err.message });
-// });
+if (true) {
+  console.log(__dirname);
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => res.send("API Running"));
+}
 
-app.get("/", (req, res) => res.send("API Running"));
 mongoose.set("useFindAndModify", false);
 const PORT = process.env.PORT || 5000;
 mongoose
