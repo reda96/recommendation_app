d7a3aa85e87222cdb4d0dc8cdabc157260e634de
Ahmed Fathi <ahmed.fathi@AssetDev.com>
2023-08-26 21:04:08 +0300

    change backend url

--- client/src/axios-orders.js
+++ client/src/axios-orders.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL:"http://localhost:5000/",
+  baseURL:"https://recommend-movie-app.onrender.com"
+  //"http://localhost:5000/",
 //   "https://cryptic-harbor-56450.herokuapp.com/",
 });
 
